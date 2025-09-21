68f4026473535b96cbf2773999a82569ec6a93ae
Kenmbal <kenmabal2905@gmail.com>
2025-09-21 22:06:33 +0800

    Created TEST_002

--- tests/TestCases.spec.ts
+++ tests/TestCases.spec.ts
@@ -5,13 +5,38 @@ test('Swag Labs Test', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
-  
-  //use data set instead of id
+
+});
+ 
+test('TEST_001', async ({ page }) => {
+//Open the website
+  await page.goto('https://www.saucedemo.com/');
+
+//use data set instead of id
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
   
+  //create a variable for btn
+  const login = page.locator('[data-test="login-button"]');
+  await login.click();
+
+  await page.screenshot({ path: 'Login.png', fullPage:true});
+
   //Sleep
   await page.waitForTimeout(5000); // Waits for 5 seconds
 
-});
+})
 
+test('TEST_002', async({page}) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('Test1');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  
+  const login = page.locator('[data-test="login-button"]');
+  await login.click();
+
+  await page.screenshot({ path:'InvalidCredentials.png', fullPage:true})
+
+  await page.waitForTimeout(3000);
+})
