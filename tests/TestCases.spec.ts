9dfe0a03fb6a3d7532a61e913ed68dc63a578b65
Kenmbal <kenmabal2905@gmail.com>
2025-09-25 00:00:49 +0800

    Added TEST_007

--- tests/TestCases.spec.ts
+++ tests/TestCases.spec.ts
@@ -102,3 +102,29 @@ test('TEST_006', async({page}) => {
 
   await page.waitForTimeout(5000);
 })
+
+test('TEST_007', async({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await page.getByAltText('Sauce Labs Backpack').click(); // For images that are clickable
+
+  await page.getByRole('button', { name: 'Add to cart' }).click();
+
+  await page.locator('//*[@id="shopping_cart_container"]/a').click();
+
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  
+  await page.getByPlaceholder('First Name').fill('Sauce');
+  await page.getByPlaceholder('Last Name').fill('Demo');
+  await page.getByPlaceholder('Zip/Postal Code').fill('3535');
+  
+  await page.screenshot({ path:'EnterAddress.png', fullPage:true})
+
+  await page.waitForTimeout(5000);
+})
