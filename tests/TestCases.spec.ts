774cd0898a3f04d1c0b6c1f734c40889f27971d4
Kenmbal <kenmabal2905@gmail.com>
2025-09-26 19:29:30 +0800

    Added TEST_009

--- tests/TestCases.spec.ts
+++ tests/TestCases.spec.ts
@@ -156,3 +156,33 @@ test('TEST_008', async({page}) => {
 
   await page.waitForTimeout(5000);
 })
+
+test('TEST_009', async({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await page.getByAltText('Sauce Labs Backpack').click(); // For images that are clickable
+
+  await page.getByRole('button', { name: 'Add to cart' }).click();
+
+  await page.locator('//*[@id="shopping_cart_container"]/a').click();
+
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  
+  await page.getByPlaceholder('First Name').fill('Sauce');
+  await page.getByPlaceholder('Last Name').fill('Demo');
+  await page.getByPlaceholder('Zip/Postal Code').fill('3535');
+
+    await page.getByRole('button', { name: 'Continue' }).click(); 
+
+  await page.getByRole('button', { name: 'Finish' }).click();
+  
+  await page.screenshot({ path:'Checkout.png', fullPage:true})
+
+  await page.waitForTimeout(5000);
+})
